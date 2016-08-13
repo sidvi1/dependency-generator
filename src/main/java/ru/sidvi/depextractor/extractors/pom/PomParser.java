e0b297dee014abf91fc1e3cedf90face816fe252
sidvi <m00n.post@gmail.com>
2016-08-13 22:03:37 +0300

    Refactoring PomParser to reduce complexity

--- src/main/java/ru/sidvi/depextractor/extractors/pom/PomParser.java
+++ src/main/java/ru/sidvi/depextractor/extractors/pom/PomParser.java
@@ -23,18 +23,7 @@ public class PomParser {
 
     private Logger logger = LoggerFactory.getLogger(PomParser.class);
 
-    private List<Tag> matchers = new ArrayList<>();
-
-    {
-        SimpleTag parent = new SimpleTag("parent", 2);
-        matchers.add(parent);
-        matchers.add(new ChildTagDecorator("groupId", parent, 3));
-        matchers.add(new ChildTagDecorator("artifactId", parent, 3));
-        matchers.add(new ChildTagDecorator("version", parent, 3));
-        matchers.add(new SimpleTag("groupId", 2));
-        matchers.add(new SimpleTag("artifactId", 2));
-        matchers.add(new SimpleTag("version", 2));
-    }
+    ExtractionHolder holder = new ExtractionHolder();
 
     public String getVersion() {
         return getValue("version");
@@ -64,58 +53,89 @@ public class PomParser {
 
         XMLInputFactory factory = XMLInputFactory.newInstance();
 
-        try (InputStreamReader reader = new InputStreamReader(is, "UTF-8")){
+        try (InputStreamReader reader = new InputStreamReader(is, "UTF-8")) {
 
-            XMLEventReader r  = factory.createXMLEventReader("UTF-8", reader);
-            LevelHolder level = new LevelHolder();
+            XMLEventReader r = factory.createXMLEventReader("UTF-8", reader);
             while (r.hasNext()) {
-                XMLEvent e = null;
-                e = r.nextEvent();
-
-                switch (e.getEventType()) {
-                    case XMLStreamConstants.START_ELEMENT: {
-                        level.up();
-
-                        StartElement el = e.asStartElement();
-                        String tagName = el.getName().getLocalPart();
-
-                        for (Tag tag : matchers) {
-                            tag.checkForStart(tagName, level);
-                        }
-                    }
-                    break;
-                    case XMLStreamConstants.CHARACTERS: {
-                        for (Tag tag : matchers) {
-                            tag.assignIfStarted(e.asCharacters().getData());
-                        }
-                    }
-                    break;
-                    case XMLStreamConstants.END_ELEMENT: {
-
-                        EndElement el = e.asEndElement();
-                        String tagName = el.getName().getLocalPart();
-
-                        for (Tag tag : matchers) {
-                            tag.checkForEnd(tagName, level);
-                        }
-
-                        level.down();
-                    }
-                    break;
-                }
+                XMLEvent e = r.nextEvent();
+                actionForEvent(e, e.getEventType());
             }
         } catch (XMLStreamException | IOException e) {
             logger.error("Error while parsing.", e);
         }
     }
 
-    private String getValue(String tagName) {
-        for (Tag matcher : matchers) {
-            if (matcher.getName().equals(tagName)) {
-                return matcher.getValue();
+    private void actionForEvent(XMLEvent e, int eventType) {
+        switch (eventType) {
+            case XMLStreamConstants.START_ELEMENT: {
+                holder.startElement(e);
             }
+            break;
+            case XMLStreamConstants.CHARACTERS: {
+                holder.characters(e);
+            }
+            break;
+            case XMLStreamConstants.END_ELEMENT: {
+                holder.endElement(e);
+            }
+            break;
         }
-        return "";
     }
 
+    private static class ExtractionHolder {
+        private LevelHolder level = new LevelHolder();
+
+        private List<Tag> matchers = new ArrayList<>();
+        {
+            SimpleTag parent = new SimpleTag("parent", 2);
+            matchers.add(parent);
+            matchers.add(new ChildTagDecorator("groupId", parent, 3));
+            matchers.add(new ChildTagDecorator("artifactId", parent, 3));
+            matchers.add(new ChildTagDecorator("version", parent, 3));
+            matchers.add(new SimpleTag("groupId", 2));
+            matchers.add(new SimpleTag("artifactId", 2));
+            matchers.add(new SimpleTag("version", 2));
+        }
+
+        private void characters(XMLEvent e) {
+            for (Tag tag : matchers) {
+                tag.assignIfStarted(e.asCharacters().getData());
+            }
+        }
+
+        private void endElement(XMLEvent e) {
+            EndElement el = e.asEndElement();
+            String tagName = el.getName().getLocalPart();
+
+            for (Tag tag : matchers) {
+                tag.checkForEnd(tagName, level);
+            }
+
+            level.down();
+        }
+
+        private void startElement(XMLEvent e) {
+            level.up();
+
+            StartElement el = e.asStartElement();
+            String tagName = el.getName().getLocalPart();
+
+            for (Tag tag : matchers) {
+                tag.checkForStart(tagName, level);
+            }
+        }
+
+        private String getValue(String tagName) {
+            for (Tag matcher : matchers) {
+                if (matcher.getName().equals(tagName)) {
+                    return matcher.getValue();
+                }
+            }
+            return "";
+        }
+    }
+
+    private String getValue(String tagName) {
+        return holder.getValue(tagName);
+    }
 }
